e956c8c55b5be0e29ba03b3d1496233b43b2a6da
Noriega29 <mateo2012ssj@hotmail.com>
2024-03-01 14:48:50 -0500

    Auto chances
    Cambios hechos por el IDE al abrir el proyecto en la nueva ruta.

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f95a8344083332f7bcde1f4f4aa7c84466b722")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
